6f4bceaca2db2de3cd0c71c55ef3be243c0517ab
Renchen Sun <renchen@amazon.com>
2020-03-06 15:50:38 -0800

    release

---
+++ docs/build/precache-manifest.5f0de75dbe7e0c741d802d396daaa6ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18693bebc2922cf49bf518e3dfd2f99a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5f11ec035d8e3abb009",
+    "url": "/static/css/main.5b4a9ba7.chunk.css"
+  },
+  {
+    "revision": "4b6f18363997f4a5b3cf",
+    "url": "/static/js/2.c6016b6d.chunk.js"
+  },
+  {
+    "revision": "38467b1af866b2deae9869fa84ba0505",
+    "url": "/static/js/2.c6016b6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5f11ec035d8e3abb009",
+    "url": "/static/js/main.2f8da285.chunk.js"
+  },
+  {
+    "revision": "c6c3aa21a26705dcc7db",
+    "url": "/static/js/runtime-main.e103f4cf.js"
+  }
+]);
