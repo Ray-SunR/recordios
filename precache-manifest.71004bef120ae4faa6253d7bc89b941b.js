af6ab2d378a3905cf638b6e05f0da2c4350a6316
Renchen Sun <renchen@amazon.com>
2020-03-06 19:24:50 -0800

    Updates

---
+++ precache-manifest.71004bef120ae4faa6253d7bc89b941b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc6b5824cf5dd0759bdeb2205ec9d474",
+    "url": "/recordios/index.html"
+  },
+  {
+    "revision": "c5f11ec035d8e3abb009",
+    "url": "/recordios/static/css/main.5b4a9ba7.chunk.css"
+  },
+  {
+    "revision": "62ee6bcb64a842a52426",
+    "url": "/recordios/static/js/2.00198ebe.chunk.js"
+  },
+  {
+    "revision": "38467b1af866b2deae9869fa84ba0505",
+    "url": "/recordios/static/js/2.00198ebe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5f11ec035d8e3abb009",
+    "url": "/recordios/static/js/main.2f8da285.chunk.js"
+  },
+  {
+    "revision": "980048d33bf5f09a68ce",
+    "url": "/recordios/static/js/runtime-main.31d42161.js"
+  }
+]);
